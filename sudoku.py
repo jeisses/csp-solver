6935f27c73f680df7997c5237a2e5966215fd318
Waldo11 <oz_hotz@yahoo.co.uk>
2015-10-01 18:27:39 +0200

    Starting baord assignments

--- sudoku.py
+++ sudoku.py
@@ -7,8 +7,26 @@ def create_constraints():
     """Create sudoku constraints"""
     col_cons = [("ALL_DIFF", ["x%s%s"%(row,col) for col in range(1, 10)]) for row in range(1,10)]
     row_cons = [("ALL_DIFF", ["x%s%s"%(col,row) for col in range(1, 10)]) for row in range(1,10)]
-        box_cons = []
+    box_cons = []
     for horiz in range(3):
         for vert in range(3):
             box_cons.append(("ALL_DIFF", ["x%s%s"%(col,row) for col in range(vert*3 + 1, vert*3 + 4) for row in range(horiz*3 + 1, horiz*3 + 4)]))
     return col_cons + row_cons + box_cons
+
+
+board1 = "............942.8.16.....29........89.6.....14..25......4.......2...8.9..5....7.."
+
+def assign(board):
+    startingAssignments = {}
+    counter = 0
+    for item in board:
+        if item != '.':
+            row = counter/9
+            col = counter%9
+            variableName = 'x' + str(row + 1) + str(col + 1)
+            startingAssignments[variableName] = board[counter]
+        counter += 1
+    return startingAssignments
+
+
+print assign(board1)   
