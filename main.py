02bf13a7767013885136942c1f295d0d5aaf3683
Jesse <jesse.eisses@gmail.com>
2015-10-07 10:38:37 +0200

    Improve interface
    - Command line arguments
    - Save solutions
    - Progress bar

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import csp, sudoku
-import time
+import time, sys
 from copy import deepcopy
 
+# Parse args
+if len(sys.argv) < 3:
+    print "Please use with 2 arguments, e.g. run_csp sudokus.txt solutions.txt"
+    sys.exit()
+
+file_in = sys.argv[1]
+file_out = sys.argv[2]
 
 # Setup 
 csp.constraints = sudoku.create_constraints(10)
@@ -14,17 +21,28 @@ times = []
 
 print "Start solving the sample file..."
 
-with open("sudokus.txt") as f:
-    for line in f:
+start = time.time()
+num_lines = sum(1 for line in open(file_in, "r")) * 1.0
+update_step = max(int(num_lines / 100), 1)
+with open(file_in, "r") as f_in, open(file_out, "w") as f_out:
+    for idx, line in enumerate(f_in):
         assignment = deepcopy(sudoku.start_assign(line))
         domains = deepcopy(sudoku_domains)
 
-        start = time.time()
+        local_start = time.time()
         solution = csp.solve(assignment, domains)
-        duration = time.time() - start
+        duration = time.time() - local_start
         times.append(duration) 
 
-        print "Done! Solution found in %s seconds:"%duration
-        sudoku.print_board(solution)
+        if idx % update_step == 0:
+            progress = int(idx / num_lines * 100)
 
+            sys.stdout.write('\r[{0}] {1}%'.format('#'*int(progress/10), progress))
+            sys.stdout.flush()
+
+        f_out.write(''.join([str(v) for v in solution.values()]))
+        f_out.write("\n")
+
+duration = time.time() - start
+print "\nSolved in %s seconds. Check %s for results"%(duration,file_out)
 
